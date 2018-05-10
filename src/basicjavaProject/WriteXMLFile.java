37101aea1c05ee2c1d9c34659b0d2c18a8fa5287
jitsolution19 <jitsolution19@gmail.com>
2018-05-10 21:51:22 +0530

    updates

--- src/basicjavaProject/WriteXMLFile.java
+++ src/basicjavaProject/WriteXMLFile.java
@@ -17,54 +17,54 @@ import org.w3c.dom.Element;
 public class WriteXMLFile {
 
 	public static void main(String[] args) {
-
+		ExcelConnection objexcelvalue = new ExcelConnection("C:\\Users\\mamta\\Desktop\\scenario.xls");
+		String value= objexcelvalue.GetDatafromExcel("Sheet1", 1, "StockName_UI");
 		try {
 
 			DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
 			DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
-
 			
 			// root elements
 			Document doc = docBuilder.newDocument();
 			Element rootElement = doc.createElement("Equity");
 			doc.appendChild(rootElement);
-
-			for(int i=1;i<10;i++)
+			String [] data= {"ABC","DEF","GHI","JKL","MNO"};
+			for(int i=1;i<5;i++)
 			{
-					
+				Element staff = doc.createElement(data[i]);
+				rootElement.appendChild(staff);	
 			}
 			// staff elements
-			Element staff = doc.createElement("BHEL");
-			rootElement.appendChild(staff);
-
-			// set attribute to staff element
-			Attr attr = doc.createAttribute("ISIN_Number");
-			attr.setValue("1");
-			staff.setAttributeNode(attr);
-
-			// shorten way
-			// staff.setAttribute("id", "1");
-
-			// firstname elements
-			Element firstname = doc.createElement("WeekHigh52");
-			firstname.appendChild(doc.createTextNode("473.50"));
-			staff.appendChild(firstname);
-
-			// lastname elements
-			Element lastname = doc.createElement("Weeklow52");
-			lastname.appendChild(doc.createTextNode("210.50"));
-			staff.appendChild(lastname);
-
-			// nickname elements
-			Element nickname = doc.createElement("MarketCap");
-			nickname.appendChild(doc.createTextNode("500000"));
-			staff.appendChild(nickname);
-
-			// salary elements
-			Element salary = doc.createElement("Beta");
-			salary.appendChild(doc.createTextNode("1.2"));
-			staff.appendChild(salary);
-
+			
+//
+//			// set attribute to staff element
+//			Attr attr = doc.createAttribute("ISIN_Number");
+//			attr.setValue("1");
+//			staff.setAttributeNode(attr);
+//
+//			// shorten way
+//			// staff.setAttribute("id", "1");
+//
+//			// firstname elements
+//			Element firstname = doc.createElement("WeekHigh52");
+//			firstname.appendChild(doc.createTextNode("473.50"));
+//			staff.appendChild(firstname);
+//
+//			// lastname elements
+//			Element lastname = doc.createElement("Weeklow52");
+//			lastname.appendChild(doc.createTextNode("210.50"));
+//			staff.appendChild(lastname);
+//
+//			// nickname elements
+//			Element nickname = doc.createElement("MarketCap");
+//			nickname.appendChild(doc.createTextNode("500000"));
+//			staff.appendChild(nickname);
+//
+//			// salary elements
+//			Element salary = doc.createElement("Beta");
+//			salary.appendChild(doc.createTextNode("1.2"));
+//			staff.appendChild(salary);
+//
 			// write the content into xml file
 			TransformerFactory transformerFactory = TransformerFactory.newInstance();
 			Transformer transformer = transformerFactory.newTransformer();
