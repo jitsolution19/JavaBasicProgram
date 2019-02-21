fcf50fc826cfef0b44985667d23cab322cc3ee5f
Jitendra <jitsolution19@gmail.com>
2019-02-22 00:16:50 +0530

    updated

--- Collections/collections/PrintNumber_Using_Collection.java
+++ Collections/collections/PrintNumber_Using_Collection.java
@@ -5,16 +5,19 @@ import java.util.ArrayList;
 public class PrintNumber_Using_Collection {
 
 	public static void main(String[] args) {
-	
-		ArrayList<Integer> temp= new ArrayList<>();
-		int i =0;
-		while(i<10)
-		{
+
+		ArrayList<Integer> temp = new ArrayList<>();
+		int i = 0;
+		while (i < 100) {
 			temp.add(i);
 			i++;
 		}
 
-		temp.forEach((n)->System.out.println("NUmber is as :: "+n));
-	}
+		temp.forEach((n) -> System.out.println("NUmber is as :: " + n));
+		temp.forEach((n) -> {
+			if (n % 2 == 0)
+				System.out.println("Even NUmber is as :: " + n);
+		});
 
+	}
 }
